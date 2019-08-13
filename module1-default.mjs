a699dd4e88f1c9dde1f5b4e885712fb505aba897
Georgi Ivanov <geivano@microsoft.com>
2019-08-12 18:00:24 -0700

    ES6 loading modules fun

---
+++ module1-default.mjs
@@ -0,0 +1,4 @@
+let projectId = 199;
+let projectName = 'BuildIt-Default';
+// export default projectName;
+export { projectName as default, projectId };
--- server.mjs
+++ server.mjs
@@ -1,6 +1,24 @@
+console.log('starting in server.mjs...')
+
 import { sqrt, square } from './app';
+
 
 const a = sqrt(4);
 const b = square(2);
 console.log(a);
 console.log(b);
+
+// import { projectId, projectName } from './module1';
+// console.log(`${projectName} has id: ${projectId}`); // ReferenceError: projectId is not defined
+
+import { projectId as id, projectName as pjName } from './module1'; // SntaxError: Identifier 'projectName' has already been declared
+console.log(`${pjName} has id: ${id}`);
+
+// import projectName from './module1-default';
+import { default as myProjectDefaultName } from './module1-default';
+console.log(`Project default name: ${myProjectDefaultName}`);
+
+import * as values from './module1-default';
+console.log(values);
+
+console.log('ending in server.mjs...')
